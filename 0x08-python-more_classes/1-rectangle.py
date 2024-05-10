3101d3b7495be4d995c38081acda75a002fd0841
Mshibe700 <philasandeluthuli77@gmail.com>
2024-05-09 18:39:49 -0700

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+@prperty
+def height(self):
+    """ method that returns the value of the height
+
+    Returns:
+    height of the rectangle
+
+
+    """
+
+    return self._height
+
+@height.stter
+def height(self, value):
+    """ method that defines the height
+
+    Args:
+        value: height
+
+    Raises:
+        TypeError: if height is not an integer
+        ValueError: if height is less than zero
+
+
+    """
+
+    if not isinstance(value, int):
+        raise TypeError("height must be an integer")
+    if value < 0:
+        raise ValueError("height must be >= 0")
+    self._height = value
