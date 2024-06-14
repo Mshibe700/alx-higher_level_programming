65f73b9eeda3e16dfe5b56371aeb8df80f76679d
Mshibe700 <philasandeluthuli77@gmail.com>
2024-06-14 02:31:06 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-"""
-Module 0-add_integer
-contain one method and accept two values of int or float type
-and cast them to int before adding
-Return an int sum
-"""
+'''
+
+This module has one function that adds up 2 integers
+
+'''
+
 
 def add_integer(a, b=98):
-    """
-        Return the summation of two arguments
-    """
-    if type(a) is not int and type(a) is not float:
+    '''Return the sum of two integers or floats as integers
+
+    Args:
+        a: first argument
+        b: second argument
+
+    Returns:
+        Sum of the two arguments
+
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
+    '''
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type (b) is not float:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    else:
-        if type(a) is float:
-            a = int(a)
-        if type(b) is float:
-            b = int(b)
-        return a + b
+    return (int(a) + int(b))
